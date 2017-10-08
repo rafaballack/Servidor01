c728696519bd4b4c8b8a064c7e6a9f20f316efdf
rafaballack <faelll@hotmail.com>
2017-10-08 01:03:22 -0300

    Update install_odoo_trustcodebr.sh

--- install_odoo_trustcodebr.sh
+++ install_odoo_trustcodebr.sh
@@ -1,55 +1,36 @@
 
 
-echo "==== Instalação dependências pip para os módulos ===="
-sudo -H pip install --upgrade pip
-sudo -H pip install --upgrade setuptools
-sudo -H pip install Babel==1.3
-sudo -H pip install Jinja2==2.7.3
-sudo -H pip install Mako==1.0.1
-sudo -H pip install MarkupSafe==0.23
-sudo -H pip install Pillow==2.7.0
-sudo -H pip install Python-Chart==1.39
-sudo -H pip install PyYAML==3.11
-sudo -H pip install Werkzeug==0.9.6
-sudo -H pip install argparse==1.2.1
-sudo -H pip install decorator==3.4.0
-sudo -H pip install docutils==0.12
-sudo -H pip install feedparser==5.1.3
-sudo -H pip install gdata==2.0.18
-sudo -H pip install gevent==1.0.2
-sudo -H pip install greenlet==0.4.7
-sudo -H pip install jcconv==0.2.3
-sudo -H pip install lxml==3.4.1
-sudo -H pip install mock==1.0.1
-sudo -H pip install ofxparse==0.14
-sudo -H pip install passlib==1.6.2
-sudo -H pip install psutil==2.2.0
-sudo -H pip install psycogreen==1.0
-sudo -H pip install psycopg2==2.5.4
-sudo -H pip install pyPdf==1.13
-sudo -H pip install pydot==1.0.2
-sudo -H pip install pyparsing==2.0.3
-sudo -H pip install pyserial==2.7
-sudo -H pip install python-dateutil==2.4.0
-sudo -H pip install python-ldap==2.4.19
-sudo -H pip install python-openid==2.2.5
-sudo -H pip install pytz==2014.10
-sudo -H pip install pyusb==1.0.0b2
-sudo -H pip install qrcode==5.1
-sudo -H pip install reportlab==3.1.44
-sudo -H pip install requests==2.6.0
-sudo -H pip install six==1.9.0
-sudo -H pip install suds-jurko==0.6
-sudo -H pip install vobject==0.6.6
-sudo -H pip install wsgiref==0.1.2
-sudo -H pip install XlsxWriter==0.7.7
-sudo -H pip install xlwt==0.7.5
-sudo -H pip install openpyxl==2.4.0-b1
-sudo -H pip install boto==2.38.0
-sudo -H pip install odoorpc
-sudo -H pip install suds_requests
-sudo -H pip install pytrustnfe
-sudo -H pip install python-boleto
-sudo -H pip install python-cnab
-sudo -H pip install http://labs.libre-entreprise.org/frs/download.php/897/pyxmlsec-0.3.1.tar.gz
-echo ">>> pip e seus requerimentos estão instalados. <<<"
+echo "Clonando repositório oficial Odoo no GitHub. Isso pode demorar um bom tempo."
+echo "Se sua internet é lenta, recomenda-se tomar um café enquanto aguarda."
+git clone https://github.com/odoo/odoo.git opt/Servidor01/odoo
+
+echo "Terminando o arquivo de configuração, quase lá."
+rm opt/Servidor01/odoo/odoo-config
+echo ""
+echo "[options]" >> opt/Servidor01/odoo/odoo-config
+echo "addons_path = addons,odoo/addons,opt/Servidor01/odoo-brasil" >> opt/Servidor01/odoo/odoo-config
+echo "admin_passwd = admin" >> opt/Servidor01/odoo/odoo-config
+echo "auto_reload = False" >> opt/Servidor01/odoo/odoo-config
+echo "csv_internal_sep = ," >> opt/Servidor01/odoo/odoo-config
+echo "db_host = localhost" >> opt/Servidor01/odoo/odoo-config
+echo "db_maxconn = 64" >> opt/Servidor01/odoo/odoo-config
+echo "db_name = False" >> opt/Servidor01/odoo/odoo-config
+echo "db_port = False" >> opt/Servidor01/odoo/odoo-config
+echo "db_template = template0" >> opt/Servidor01/odoo/odoo-config
+echo "db_user = odoo" >> opt/Servidor01/odoo/odoo-config
+echo "db_password = 123" >> opt/Servidor01/odoo/odoo-config
+
+echo "Clonando repositório oficial dos módulos Odoo Brasil no GitHub."
+echo "Agora falta pouco."
+git clone https://github.com/Trust-Code/odoo-brasil.git opt/Servidor01/odoo-brasil
+
+echo "==== Instalação e configuração Odoo Brasil completa ===="
+echo "---- PostgreSQL ---- "
+echo ">> Usuário: odoo -- Senha: 123"
+echo ">> Usuário: postgres -- Senha = 123"
+echo "---- Instância Odoo ----"
+echo "Pasta de instalação: ~/odoo"
+echo "Pasta de Addons: addons, ~/odoo/addons, ~/odoo-brasil"
+echo "========================================================"
+echo "A instalação está completa !"
+echo "Obrigado por usar este script !!!"
